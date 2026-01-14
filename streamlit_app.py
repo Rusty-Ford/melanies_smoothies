aac685b443153d37f90a58edd9b93a8a695aa41a
Rusty-Ford <rustyford8696@gmail.com>
2026-01-13 17:37:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'), col('search_on'))
-st.dataframe(data = my_dataframe, use_container_width = True)
+#st.dataframe(data = my_dataframe, use_container_width = True)
 #st.stop()
 
 # convert the snowpark dataframe to a pandas dataframe so we can use the LOC function
@@ -72,7 +72,7 @@ if ingredients_list:
   for fruit_chosen in ingredients_list:
     ingredients_string += fruit_chosen + ' '
 
-    search_on = pd_df.loc[pd_df['fruit_name'] == fruit_chosen, 'search_on'].iloc[0]
+    search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
     st.write('The search for ', fruit_chosen,' is ', search_on, '.')
 
     st.subheader(fruit_chosen + ' Nutrition Information')
